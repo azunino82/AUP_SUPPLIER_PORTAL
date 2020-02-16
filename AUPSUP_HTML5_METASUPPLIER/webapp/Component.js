3f1e3d86dc0740b15b1c4a684c481198d5d1db01
azunino <azunino@alteanet.it>
2020-02-16 21:50:59 +0100

    Modifica app html5

--- AUPSUP_HTML5_METASUPPLIER/webapp/Component.js
+++ AUPSUP_HTML5_METASUPPLIER/webapp/Component.js
@@ -29,53 +29,7 @@ sap.ui.define([
 			var userModel = new sap.ui.model.json.JSONModel("/services/userapi/currentUser");
 			sap.ui.getCore().setModel(userModel, "userapi");
 
-			try {
-
-				jQuery.sap.require("sap.ushell.cpv2.services.cloudServices.SiteService");
-
-				if (sap.ushell && sap.ushell.cpv2 && sap.ushell.cpv2.services &&
-					sap.ushell.cpv2.services.cloudServices &&
-					sap.ushell.cpv2.services.cloudServices.SiteService) {
-
-					var oLocalSiteService = sap.ushell.cpv2.services.cloudServices.SiteService();
-					var oRoles = oLocalSiteService.siteModel.getProperty("/roles");
-					var oProperty;
-					for (oProperty in oRoles) {
-						if (oRoles.hasOwnProperty(oProperty)) {
-
-							console.log("(Component) Ruolo Funzionalita: " + oProperty.toString());
-
-							if (oProperty.toString() !== "Everyone" && oProperty.toString() !== "Anonymous") {
-
-								if (oProperty.toString().startsWith("Z_RL_BUYER")) {
-									visibilityRoles.isBuyer = true;
-
-								}
-
-								if (oProperty.toString().startsWith("Z_RL_SUPPLIER")) {
-									visibilityRoles.isSupplier = true;
-								}
-
-								var oModel = new JSONModel();
-								oModel.setData(visibilityRoles);
-								this.setModel(oModel, "VisibilityJSONModel");
-
-								console.log("(Component) Ruolo: " + oProperty.toString() + " visibilityRoles: " + JSON.stringify(visibilityRoles));
-
-							}
-
-						}
-					}
-
-				}
-			} catch (oException) {
-				console.log("(Component) Ruolo CATCH");
-
-				visibilityRoles.isBuyer = true;
-				var oModel = new sap.ui.model.json.JSONModel();
-				oModel.setData(visibilityRoles);
-				this.setModel(oModel, "VisibilityJSONModel");
-			}
+			
 
 
 		}
