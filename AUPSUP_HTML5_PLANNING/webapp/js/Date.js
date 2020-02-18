daf212a5f59697a2a919cbe7d4004a6c9a19596d
azunino <azunino@alteanet.it>
2020-02-18 15:02:02 +0100

    ALL APPS aggiunta js

---
+++ AUPSUP_HTML5_PLANNING/webapp/js/Date.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+    "sap/ui/model/SimpleType",
+    "sap/ui/model/ValidateException"
+], function (SimpleType, ValidateException) {
+    "use strict";
+    
+    return SimpleType.extend("it.alteaup.supplier.portal.searchHU.type.Date", {
+    	 
+        formatValue: function(sDate) {
+
+			var oFromFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({
+				pattern: "yyyyMMdd"
+			});
+			var oDate = oFromFormat.parse(sDate, true);
+			var oToFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({
+				pattern: "dd MMM yyyy"
+			});
+			if (sDate === "00000000") {
+				return ""; // or whatever special case
+			} else {
+			var sResult = oToFormat.format(oDate);
+			return sResult;}
+        },
+        
+        parseValue: function(oValue) {
+            return oValue;
+        },
+        
+        validateValue: function(oValue) {
+        	return true;
+        }
+
+    });
+
+});
