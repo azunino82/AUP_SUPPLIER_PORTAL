02b10a1efa1f986068e19ed68c0e5dd51b45f4d4
Angelo Zunino <azunino@alteanet.it>
2020-02-27 17:34:54 +0000

    APRV SCHED AGREE new App

---
+++ AUPSUP_HTML5_APR_SCDAGR/webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("it.alteaup.supplier.portal.aprvschdagr.AUPSUP_HTML5_APR_SCDAGR.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
