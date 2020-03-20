299323fafa437ace676b289c85253e1af99068ef
laura-scardy <laura.scardilli@libero.it>
2020-03-20 10:23:04 +0100

    AUPSUP_NODE - Customizing

--- AUPSUP_NODE/it/alteaup/supplier/portal/Customizing/CustomizingManagement.js
+++ AUPSUP_NODE/it/alteaup/supplier/portal/Customizing/CustomizingManagement.js
@@ -149,15 +149,153 @@ module.exports = function () {
     })
   })
 
-  app.post('/SaveTUserIdMetaId', function (req, res) {
+  app.post('/SaveTMetaIdForn', function (req, res) {
     const body = req.body
 
-    var sql = 'UPSERT \"AUPSUP_DATABASE.data.tables::T_USERID_METAID\" VALUES (\'' + body.USERID + '\',\'' + body.METAID + '\') WITH PRIMARY KEY'
+    var sql = 'UPSERT \"AUPSUP_DATABASE.data.tables::T_METAID_FORN\" VALUES (\'' + body.METAID + '\',\'' + body.LIFNR + '\',\'' + body.SYSID + '\') WITH PRIMARY KEY'
     console.log('sql: ' + sql)
     hdbext.createConnection(req.tenantContainer, function (error, client) {
       if (error) {
-        console.error('ERROR T_USERID_METAID :' + stringifyObj(error))
-        return res.status(500).send('T_USERID_METAID CONNECTION ERROR: ' + stringifyObj(error))
+        console.error('ERROR T_METAID_FORN :' + stringifyObj(error))
+        return res.status(500).send('T_METAID_FORN CONNECTION ERROR: ' + stringifyObj(error))
+      }
+      if (client) {
+        async.waterfall([
+
+          function prepare (callback) {
+            client.prepare(sql,
+              function (err, statement) {
+                callback(null, err, statement)
+              })
+          },
+
+          function execute (_err, statement, callback) {
+            statement.exec([], function (execErr, results) {
+              callback(null, execErr, results)
+            })
+          },
+
+          function response (err, results, callback) {
+            if (err) {
+              res.type('application/json').status(500).send({ ERROR: err })
+              return
+            } else {
+              res.type('application/json').status(200).send({ results: 'OK' })
+            }
+            callback()
+          }
+        ], function done (err, parameters, rows) {
+          console.log('---->>> CLIENT END T_BUYERS <<<<<-----')
+          client.close()
+          if (err) {
+            return console.error('Done error', err)
+          }
+        })
+      }
+    })
+  })
+
+  app.post('/SaveTBuPlant', function (req, res) {
+    const body = req.body
+
+    var sql = 'UPSERT \"AUPSUP_DATABASE.data.tables::T_BU_PLANT\" VALUES (\'' + body.BU + '\',\'' + body.SYSID + '\',\'' + body.PLANT + '\',\'' + body.PLANT_DESCR + '\') WITH PRIMARY KEY'
+    console.log('sql: ' + sql)
+    hdbext.createConnection(req.tenantContainer, function (error, client) {
+      if (error) {
+        console.error('ERROR T_BU_PLANT :' + stringifyObj(error))
+        return res.status(500).send('T_BU_PLANT CONNECTION ERROR: ' + stringifyObj(error))
+      }
+      if (client) {
+        async.waterfall([
+
+          function prepare (callback) {
+            client.prepare(sql,
+              function (err, statement) {
+                callback(null, err, statement)
+              })
+          },
+
+          function execute (_err, statement, callback) {
+            statement.exec([], function (execErr, results) {
+              callback(null, execErr, results)
+            })
+          },
+
+          function response (err, results, callback) {
+            if (err) {
+              res.type('application/json').status(500).send({ ERROR: err })
+              return
+            } else {
+              res.type('application/json').status(200).send({ results: 'OK' })
+            }
+            callback()
+          }
+        ], function done (err, parameters, rows) {
+          console.log('---->>> CLIENT END T_BUYERS <<<<<-----')
+          client.close()
+          if (err) {
+            return console.error('Done error', err)
+          }
+        })
+      }
+    })
+  })
+
+  app.post('/SaveTBuPurchOrg', function (req, res) {
+    const body = req.body
+
+    var sql = 'UPSERT \"AUPSUP_DATABASE.data.tables::T_BU_PURCH_ORG\" VALUES (\'' + body.BU + '\',\'' + body.SYSID + '\',\'' + body.PURCH_ORG + '\',\'' + body.PURCH_DESCR + '\') WITH PRIMARY KEY'
+    console.log('sql: ' + sql)
+    hdbext.createConnection(req.tenantContainer, function (error, client) {
+      if (error) {
+        console.error('ERROR T_BU_PURCH_ORG :' + stringifyObj(error))
+        return res.status(500).send('T_BU_PURCH_ORG CONNECTION ERROR: ' + stringifyObj(error))
+      }
+      if (client) {
+        async.waterfall([
+
+          function prepare (callback) {
+            client.prepare(sql,
+              function (err, statement) {
+                callback(null, err, statement)
+              })
+          },
+
+          function execute (_err, statement, callback) {
+            statement.exec([], function (execErr, results) {
+              callback(null, execErr, results)
+            })
+          },
+
+          function response (err, results, callback) {
+            if (err) {
+              res.type('application/json').status(500).send({ ERROR: err })
+              return
+            } else {
+              res.type('application/json').status(200).send({ results: 'OK' })
+            }
+            callback()
+          }
+        ], function done (err, parameters, rows) {
+          console.log('---->>> CLIENT END T_BUYERS <<<<<-----')
+          client.close()
+          if (err) {
+            return console.error('Done error', err)
+          }
+        })
+      }
+    })
+  })
+
+  app.post('/SaveTProfiliConferma', function (req, res) {
+    const body = req.body
+
+    var sql = 'UPSERT \"AUPSUP_DATABASE.data.tables::T_PROFILI_CONFERMA\" VALUES (\'' + body.SYSID + '\',\'' + body.PROFILO_CONTROLLO + '\',\'' + body.CAT_CONFERMA + '\',\'' + body.DESCRIZIONE + '\',\'' + body.OWNER + '\',\'' + body.TIPO_CONFERMA + '\',\'' + body.MODIFICA_PREZZO + '\',\'' + body.PERC_INFERIORE + '\',\'' + body.PERC_SUPERIORE + '\',\'' + body.PARZIALE_QUANTITA + '\',\'' + body.PERC_INFERIORE_QUANT + '\',\'' + body.PERC_SUPERIORE_QUANT + '\',\'' + body.MODIFICA_QUANTITA + '\',\'' + body.TIPO_COND_PREZZO + '\',\'' + body.TIPO_CONSEGNA_INB + '\',\'' + body.LOTTO_FORNITORE_INB + '\',\'' + body.DATA_SCADENZA_INB + '\',\'' + body.DATA_PRODUZIONE_INB + '\',\'' + body.NUMERO_SERIALE_INB + '\',\'' + body.CONFERMA_MANDATORY + '\',\'' + body.CONTROLLO_CORSO_APP + '\',\'' + body.ZAPPPERSUP + '\',\'' + body.ZAPPPERINF + '\',\'' + body.ZAPPGGSUP + '\',\'' + body.ZAPPGGINF + '\') WITH PRIMARY KEY'
+    console.log('sql: ' + sql)
+    hdbext.createConnection(req.tenantContainer, function (error, client) {
+      if (error) {
+        console.error('ERROR T_PROFILI_CONFERMA :' + stringifyObj(error))
+        return res.status(500).send('T_PROFILI_CONFERMA CONNECTION ERROR: ' + stringifyObj(error))
       }
       if (client) {
         async.waterfall([
