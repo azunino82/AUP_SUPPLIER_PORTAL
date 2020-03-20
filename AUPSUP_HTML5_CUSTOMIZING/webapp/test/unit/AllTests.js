5293930dadc6ce571e1f7092505501d239995af2
Angelo Zunino <azunino@alteanet.it>
2020-03-20 08:09:19 +0000

    CUSTOMIZING: new

---
+++ AUPSUP_HTML5_CUSTOMIZING/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"it/alteaup/supplier/portal/customizing/AUPSUP_HTML5_CUSTOMIZING/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
---
+++ AUPSUP_HTML5_CUSTOMIZING/webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"it/alteaup/supplier/portal/customizing/AUPSUP_HTML5_CUSTOMIZING/test/unit/AllTests"
+	], function () {
+		QUnit.start();
+	});
+});
