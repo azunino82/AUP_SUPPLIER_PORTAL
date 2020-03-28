0635aafd81475be611a99fa9625dfdf16d854754
azunino <azunino@alteanet.it>
2020-03-28 21:42:52 +0100

    PURCH ORDS gestione zeri davanti a lifnr e matnr

--- AUPSUP_HTML5_PURCH_ORDS/webapp/controller/BaseController.js
+++ AUPSUP_HTML5_PURCH_ORDS/webapp/controller/BaseController.js
@@ -254,6 +254,20 @@ sap.ui.define([
 					return sValue.toString().replace('.00', '')
 				else
 					return sValue.toString().replace('.', ',')
+		},
+		
+		removeZeroBefore: function (sValue) {
+			if (sValue !== undefined) {
+				var n = 0
+				for (var i = 0; i < sValue.length; i++) {
+					if (sValue.charAt(i) !== '0') {
+						n = i
+						break
+					}
+				}
+				return sValue.substring(n, sValue.length);
+			}
+			return sValue
 		}
 
 
