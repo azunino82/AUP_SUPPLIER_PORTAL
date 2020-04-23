521da2f8dab9f5add18c0d906f42c49d1f7bc53b
azunino <azunino@alteanet.it>
2020-04-23 20:14:43 +0200

    SEARCH_HU: modifica path

--- AUPSUP_HTML5_SEARCH_HU/webapp/controller/App.controller.js
+++ AUPSUP_HTML5_SEARCH_HU/webapp/controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("it.alteaup.supplier.portal.searchHU.AUPSUP_HTML5_SEARCH_HU.controller.App", {
+	return Controller.extend("it.aupsup.searchHU.AUPSUP_HTML5_SEARCH_HU.controller.App", {
 		onInit: function () {
 
 		}
